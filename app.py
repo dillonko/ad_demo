8283e76d4def246feea05a6a2d41fead2d61da59
Dillon Omane <dillonko84@gmail.com>
2015-09-28 13:14:44 -0400

    I added som functions to get the job done faster

--- app.py
+++ app.py
@@ -28,10 +28,55 @@ def index():
 #
 #     else:
 #         return "fail"
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'example.db'),
+    DEBUG=True
+))
+
+def connect_db():
+    """connect to the specific db"""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """opens a new db connection if there is none yet for the cyrrent app"""
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+        return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """closes the db again at the end of the request."""
+if hasattr(g, 'sqlite_db'):
+    g.sqlite_db.close()
+
 @app.route('/save', methods=['PUT','POST'])
 def get_info():
-  print ("{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}".format(id, firstname, lastname, youremail, title, managersemail, startdate, account0, account1, account2, account3, account4, account5, account6, account7, account8, account9, account10, account11, account12, account13, account14, account15))
-  return request.json
+  print ("{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}".format(id,
+                                                                 firstname,
+                                                                 lastname,
+                                                                 youremail,
+                                                                 title,
+                                                                 managersemail,
+                                                                 startdate,
+                                                                 account0,
+                                                                 account1,
+                                                                 account2,
+                                                                 account3,
+                                                                 account4,
+                                                                 account5,
+                                                                 account6,
+                                                                 account7,
+                                                                 account8,
+                                                                 account9,
+                                                                 account10,
+                                                                 account11,
+                                                                 account12,
+                                                                 account13,
+                                                                 account14,
+                                                                 account15))
 
 
 if __name__ == "__main__":
